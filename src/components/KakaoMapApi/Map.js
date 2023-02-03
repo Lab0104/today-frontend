66a96978a96823bd3aeaaddedb844395683208d2
hin6150 <hin6150@gmail.com>
2023-02-03 22:27:41 +0900

    temp Kakao Map API

--- src/components/KakaoMapApi/Map.js
+++ src/components/KakaoMapApi/Map.js
@@ -5,46 +5,88 @@ import React, { useEffect, useRef, useState } from "react";
 const { kakao } = window;
 
 function Map(props) {
-  const [infowindow, setInfowindow] = useState(); //인포 윈도우
-  const [kakaoMap, setKakaoMap] = useState(null); //지도 객체
-  const [markers, setmarkers] = useState([]); //마커 집단
+  const [kakaoMap, setKakaoMap] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [infowindow, setInfowindow] = useState(
+    new kakao.maps.InfoWindow({ zIndex: 1 })
+  );
   const mapContainer = useRef(null);
 
   useEffect(() => {
     initMap();
   }, []);
 
+  useEffect(() => {
+    searchDB();
+  }, [props.keyword]);
+
   const initMap = () => {
     //좌표값
     const center = new kakao.maps.LatLng(37.566826, 126.9786567);
     const options = {
       center,
-      level: 3,
+      level: 5,
     };
     setKakaoMap(new kakao.maps.Map(mapContainer.current, options));
   };
 
-  return (
-    <div css={mapStyle} ref={mapContainer}>
-      <InitMarker position={{ lat: 37.566826, lng: 126.9786567 }} />
-    </div>
-  );
-}
-const InitMarker = (props) => {
-  let marker = new kakao.maps.Marker({
-    position: new kakao.maps.LatLng(
-      props.position.latitude,
-      props.position.longitude
-    ),
-    title: props.position.longitude,
-    image: null,
-    clickable: true,
-  });
+  const searchDB = () => {
+    if (!kakaoMap) return;
+    if (props.keyword === "") return;
 
-  marker.setMap(props.kakaoMap);
-};
+    infowindow.close();
+
+    markers.map((marker, idx) => {
+      marker.setMap(null);
+    });
+    setMarkers([]);
+
+    // 장소 검색 객체를 생성합니다
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(props.keyword, placesSearchDB);
+
+    function placesSearchDB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        props.setSearchData([]);
+        props.setSearchData([...data]);
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        const bounds = new kakao.maps.LatLngBounds();
+
+        data.map((place, idx) => {
+          const marker = new kakao.maps.Marker({
+            map: kakaoMap,
+            position: new kakao.maps.LatLng(place.y, place.x),
+          });
+
+          markers.push(marker);
+          setMarkers([...markers]);
+
+          bounds.extend(new kakao.maps.LatLng(place.y, place.x));
+
+          kakao.maps.event.addListener(marker, "click", function () {
+            console.log(place.place_name);
+            infowindow.setContent(
+              `<div style="padding:5px;">${place.place_name}</div>`
+            );
+
+            infowindow.open(kakaoMap, marker);
+          });
+        });
+
+        kakaoMap.setBounds(bounds);
+      }
+    }
+  };
+
+  return <div css={mapStyle} ref={mapContainer}></div>;
+}
+
 const mapStyle = css`
   width: 100%;
   height: 100vh;
 `;
+
 export default Map;
