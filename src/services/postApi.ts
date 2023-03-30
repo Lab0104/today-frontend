dfeb54212b3e390da828313a44bee41cd5e6f766
hin6150 <hin6150@gmail.com>
2023-03-30 14:57:37 +0900

    test DB API

---
+++ src/services/postApi.ts
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postApi = createApi({
+  reducerPath: "postApi", // 없는 경우는 api가 디폴트로 정해진다.
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  // 재 랜드링을 위한 테그 설정. RTK Query의 기본 랜드링은 tag시스템으로 동작한다.
+  // 만약 자료가 수정되었을 때 Tag를 invalides로 설정하면 해당 tag가 설정된
+  // 캐쉬 자료를 리플레시하는 기본 tag 이름 정의 , 예 ['Users', 'Posts']
+  tagTypes: ["Post"],
+
+  //각각의 query(get)/mutation(set) 동작을 여기서 정의한다.
+  endpoints: (builder) => ({
+    // build.query는 get에 해당되는 query이다.
+    getPost: builder.query({
+      // id는 호출에서 받은 값.
+      query: ({ name }) => `${name}`,
+      // query: (id) => ({ url: `post/${id}` }),
+      // arg는 호출하는 곳에 전달한 기본 parameter 값, 여기서는 id
+
+      // 쿼리 결과에 tag을 주입한다. 나중에 invalidesTags로 재 호출/랜드링한다.
+      providesTags: (result, error, arg) => [{ type: "Post" }],
+    }),
+
+    // mutation은 데이터 조작을 정의한다, POST, PUT, DELETE, PATCH
+    updatePost: builder.mutation({
+      query: ({ name, value }) => ({
+        url: `${name}`,
+        method: "POST",
+        body: { value },
+      }),
+
+      //Post로 등록된 모든 캐쉬 내용을 업데이트, tag에 따라 수정한 곳만 업데이트 가능.
+      // [{type:'Post', id}]가 등록되어 있고 이 것을 invalidatesTags로 호출하면 행당 id만 재 호출/랜드링.
+      invalidatesTags: (result, error, arg) => [{ type: "Post" }],
+    }),
+  }),
+});
+//정의된 endpoints을 기반으로 자동 생성됨. getPost=>useGetPostQuery
+export const { useGetPostQuery, useUpdatePostMutation } = postApi;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import DisplayMeetingSlice from "../reducer/DisplayMeetingSlice";
 import KakaoMapSlice from "../reducer/KakaoMapSlice";
 import MeetingCardSlice from "../reducer/MeetingCardSlice";
 import ModalSlice from "../reducer/ModalSlice";
 import ToggleSlice from "../reducer/ToggleSlice";
 import userSlice from "../reducer/userSlice";
+import { postApi } from "../services/postApi";
 
 export const store = configureStore({
   reducer: {
@@ -14,8 +16,16 @@ export const store = configureStore({
     map: KakaoMapSlice, // 카카오 맵
     display: DisplayMeetingSlice, // 카카오맵의 검색 결과
     toggle: ToggleSlice, // 토글
+    [postApi.reducerPath]: postApi.reducer,
   },
+  // API 미들웨어를 추가하면 cahsing, invalidation, polling, 기타 유용한 기능 사용 가능.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postApi.middleware),
 });
+
+// 옵셔널, refetchOnFocus/refetchOnReconnect 기능을 위해 필요함
+// setupListeners 문서를 참고 - 커스텀을 위한 옵셔널 콜백을 2번째 인자로 받음
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
