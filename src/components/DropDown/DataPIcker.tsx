0012bcf19e40f7c12641287904d051db288ddb4e
hin6150 <hin6150@gmail.com>
2023-04-26 12:27:27 +0900

    Feat 모임 추가 세부 기능 완료

--- src/components/DropDown/DataPIcker.tsx
+++ src/components/DropDown/DataPIcker.tsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
-import setHours from "date-fns/setHours";
-import setMinutes from "date-fns/setMinutes";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/esm/locale";
 import styled from "@emotion/styled";
 
-const DatePicker = (): any => {
+const DatePicker = (props: any): any => {
   const [startDate, setStartDate] = useState(null);
 
   return (
     <CustomDatePicker
-      placeholderText="날짜"
+      placeholderText={props.text}
       selected={startDate}
       onChange={(date: any) => setStartDate(date)}
       showTimeSelect
       locale={ko}
       timeFormat="HH:mm"
       timeIntervals={15}
+      // dateFormat={props.time ? "h:mm aa" : "yyyy. M. d h:mm aa"}
       dateFormat="yyyy. M. d h:mm aa"
       minDate={new Date()}
       showDisabledMonthNavigation
--- src/components/Modal/AddModal.tsx
+++ src/components/Modal/AddModal.tsx
@@ -1,10 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import styled from "@emotion/styled";
 import { useState } from "react";
 
 import { useDispatch } from "react-redux";
-import { useForm } from "react-hook-form";
 import { closeModal } from "../../reducer/ModalSlice";
 import { toggleButtons } from "../../reducer/ToggleSlice";
 import { Container, ExitButton, Content } from "./CommonStyles";
@@ -16,29 +14,17 @@ import {
   BsCalendar2,
   BsPeopleFill,
   BsChevronDown,
+  BsChevronUp,
 } from "react-icons/bs";
 import DatePicker from "components/DropDown/DataPIcker";
+
 import { categories } from "components/CategoryList/CategoryList";
 
 const AddModal = () => {
   const dispatch = useDispatch();
-  // const [inputData, setInputData] = useState({
-  //   postId: "",
-  //   title: "",
-  //   subTitle: "",
-  //   category: "",
-  //   currentMember: "",
-  //   recruitments: "",
-  //   address: "",
-  //   date: "",
-  //   deadLine: "",
-  //   content: "",
-  //   user: "",
-  //   boardHits: 0,
-  //   position: "",
-  // });
 
   const [address, setAddress] = useState("주소");
+  const [category, setCategory] = useState(["모임 카테고리", ">", ""]);
 
   const [view, setView] = useState(false);
 
@@ -47,17 +33,33 @@ const AddModal = () => {
   //   setInputData({ ...inputData, [className]: value });
   // };
 
+  const tagHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    // if (e.target.value[0] !== "#") e.target.value = "#" + e.target.value;
+  };
+
   const Dropdown = (): JSX.Element => {
     return (
       <div css={dropbox}>
         <ul>
-          {categories.map((data) => {
+          {categories.map((data, idx1) => {
             return (
-              <div className="wrapper">
-                <li className="data-name">{data.name}</li>
-                <div className="data-list">
-                  {data.list.map((list) => {
-                    return <li>{list}</li>;
+              <div className="wrapper" key={"box" + idx1}>
+                <li className="data-name" key={"name" + idx1}>
+                  {data.name}
+                </li>
+                <div className="data-list" key={"list" + idx1}>
+                  {data.list.map((list, idx2) => {
+                    return (
+                      <li
+                        key={idx2}
+                        onClick={() => {
+                          setCategory([data.name, " > ", list]);
+                        }}
+                      >
+                        {list}
+                      </li>
+                    );
                   })}
                 </div>
               </div>
@@ -70,7 +72,7 @@ const AddModal = () => {
 
   const dropbox = css`
     position: absolute;
-    left: 70px;
+    left: 30px;
     top: 48px;
     background-color: #fff;
     width: 150px;
@@ -86,13 +88,15 @@ const AddModal = () => {
       padding: 10px;
     }
     li:hover {
-      cursor: pointer;
       background-color: #eee;
+      cursor: pointer;
     }
     .wrapper {
       position: relative;
     }
-
+    .wrapper:hover .data-name {
+      background-color: #eee;
+    }
     .data-list {
       position: absolute;
       background-color: #fff;
@@ -101,9 +105,10 @@ const AddModal = () => {
       top: 0;
       left: 100%;
       display: none;
-      width: 200px;
+      width: 250px;
       padding: 10px 0;
     }
+
     .wrapper:hover .data-list {
       display: block;
     }
@@ -165,30 +170,32 @@ const AddModal = () => {
           <div className="input-title">
             <input placeholder="모임 제목"></input>
           </div>
-          <div className="input-category">
-            <div className="category large">
-              <p>모임 카테고리</p>
-              <BsChevronDown
-                className="drop-down"
-                onClick={() => {
-                  setView(!view);
-                }}
-              />
-              {view && <Dropdown></Dropdown>}
-            </div>
-            <div className="category sub">
-              <p>모임 카테고리</p>
+          <div
+            className="category"
+            onClick={() => {
+              setView(!view);
+            }}
+          >
+            <p>{category[2] === "" ? category[0] : category}</p>
+            {view ? (
+              <BsChevronUp className="drop-down" />
+            ) : (
               <BsChevronDown className="drop-down" />
-            </div>
+            )}
+            {view && <Dropdown></Dropdown>}
           </div>
           <div className="input-info">
             <div className="people info-box">
               <BsPeopleFill />
-              <input placeholder="인원 수" type="number"></input>
+              <input placeholder="인원 수" type="number" min="2"></input>
             </div>
             <div className="date info-box">
-              <BsCalendar2 />
-              <DatePicker />
+              <div>
+                <BsCalendar2 />
+              </div>
+              <DatePicker text="모임 시작시간" />
+              <p>~</p>
+              <DatePicker text="모임 종료시간" />
             </div>
             <div className="place info-box">
               <BsMap />
@@ -202,15 +209,16 @@ const AddModal = () => {
             </div>
             <div className="deadline info-box">
               <BsAlarm />
-              <DatePicker />
+              <DatePicker text="모집 마감 날짜" />
             </div>
           </div>
           <div className="input-content">
             <textarea placeholder="모임 내용"></textarea>
             <div className="tag">
-              <p>#태그</p>
-              <p>#태그</p>
-              <p>#태그</p>
+              <input placeholder="#태그" maxLength={4} onChange={tagHandle} />
+              <input placeholder="#태그" />
+              <input placeholder="#태그" />
+              <input placeholder="#태그" />
             </div>
           </div>
         </div>
@@ -253,27 +261,19 @@ const ContentBox = css`
     line-height: 16px;
   }
 
-  & .input-category {
-    display: flex;
-    gap: 10px;
-  }
-
-  & .input-category input::placeholder,
-  .input-title input::placeholder {
+  & .input-title input::placeholder {
     color: #000;
     font-weight: 500;
   }
-  & .input-category .category {
+  & .category {
     position: relative;
     background-color: #fff;
-    width: 50%;
     border-radius: 5px;
     padding: 0 30px;
+    cursor: pointer;
   }
-  & .input-category .category p {
-    color: #000;
-  }
-  & .input-category .drop-down {
+
+  & .category .drop-down {
     position: absolute;
 
     width: 16px;
@@ -282,6 +282,9 @@ const ContentBox = css`
     top: 0;
     bottom: 0;
     margin: auto;
+  }
+  & .date p {
+    padding-right: 10px;
   }
 
   & .input-info {
@@ -343,7 +346,7 @@ const ContentBox = css`
     gap: 10px;
   }
 
-  & .input-content .tag p {
+  & .input-content .tag input {
     height: 36px;
     font-size: 14px;
     background: #eee;
@@ -368,56 +371,3 @@ const ContentBox = css`
 `;
 
 export default AddModal;
-
-/* 
-# 카테고리
-
-1. 학문/스터디
-    - 프로그래밍 언어 스터디
-    - 영어 회화 스터디
-    - 디자인 스터디
-    - 수학 스터디
-    - 과학 실험 스터디
-2. 비즈니스
-    - 창업 컨설팅 그룹
-    - 경영 전략 모임
-    - 마케팅 교육 모임
-    - 취업 멘토링 그룹
-    - 글로벌 비즈니스 네트워크 그룹
-3. 예술/문화
-    - 문학 작품 독서 그룹
-    - 미술 전시 관람 그룹
-    - 콘서트, 연극 관람 모임
-    - 필름, 영화 관람 그룹
-    - 문화유산 탐방 모임
-4. 스포츠/게임
-    - 축구 클럽
-    - 테니스 동호회
-    - 보드 게임 그룹
-    - 카드 게임 모임
-    - 게임 개발 동아리
-5. 사회활동/자선
-    - 장애인 돕기 봉사 모임
-    - 동물보호 활동 그룹
-    - 청소년 지원 봉사 모임
-    - 글로벌 기아 해결 운동
-    - 지역 사회 문제 해결 그룹
-6. 요리/음식
-    - 베이킹 동호회
-    - 쿠킹 클래스 그룹
-    - 다양한 음식 맛보기 모임
-    - 정기적인 레스토랑 방문 그룹
-    - 지역 푸드 투어 그룹
-7. 여행/문화 탐방
-    - 해외 여행 그룹
-    - 국내 여행 그룹
-    - 여행사 투어 그룹
-    - 유적지 탐방 모임
-    - 문화 축제 탐방 그룹
-8. 수공예
-    - 뜨개질 동호회
-    - 도예 작업실 그룹
-    - 목공 예술 동아리
-    - 수채화, 디자인 스케치 모임
-    - 섬유 미술 작업실 그룹
-*/
