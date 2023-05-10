e8e31537e1e6f74bba7c85371182abd0a8063e7e
krdevSangwoo <34301263+krdevSangwoo@users.noreply.github.com>
2023-05-10 11:08:43 +0900

    Update dataKakaoLogin.js redirect_uri

--- src/pages/Login/dataKakaoLogin.js
+++ src/pages/Login/dataKakaoLogin.js
@@ -1,5 +1,5 @@
 // 향후 보안 측면에서 환경변수(.env)로 전환해야함
 
 export const REST_API_KEY = "3c50f4184948d9d85094617a392c36ee";
-export const REDIRECT_URI = "http://localhost:3000/kakaoLogin";
+export const REDIRECT_URI = "https://today-frontend-seven.vercel.app/kakaoLogin";
 export const CLIENT_SECRET = "Noo0yJlqx9HL83w6wy0DSgBYnlXsTBoW";
